17485957c0e56d54d96e2d75b1aac38074a1d850
DongJun <DongJun@192.168.0.115>
2017-07-05 13:09:56 +0900

    동준 커밋푸쉬

---
+++ project_mugs/src/com/mugs/dao/impl/RoomDaoImpl.java
@@ -0,0 +1,51 @@
+package com.mugs.dao.impl;
+
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.mugs.dao.RoomDao;
+import com.mugs.vo.Room;
+
+@Repository
+public class RoomDaoImpl implements RoomDao {
+	
+	@Autowired
+	private SqlSessionTemplate session;
+
+	private String makeSql(String id){
+		return "com.mugs.config.mapper.roomMapper."+id;
+	}
+
+	@Override
+	public int insertRoom(Room room) {
+		return session.insert(makeSql("insertRoom"),room);
+	}
+
+	@Override
+	public int updateRoom(Room room) {
+		return session.update(makeSql("updateRoom"), room);
+	}
+
+	@Override
+	public int deleteRoom(int roomId) {
+		return session.delete(makeSql("deleteRoom"), roomId);
+	}
+
+	@Override
+	public Room selectRoomById(int roomId) {
+		return session.selectOne(makeSql("selectRoomById"), roomId);
+	}
+
+	@Override
+	public List<Room> selectRoomList() {
+		return session.selectList(makeSql("selectRoomList"));
+	}
+
+	@Override
+	public List<Room> selectRoomByReference(int buildingId) {
+		return session.selectList(makeSql("selectRoomByReference"), buildingId);
+	}
+}
